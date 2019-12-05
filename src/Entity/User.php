fe5c036dd2cba7be037d5b533340afeea54c4dea
Irene <ireneog_72@hotmail.es>
2019-12-05 20:01:09 +0100

    Finalize ORM relations

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -2,6 +2,9 @@
 
 namespace App\Entity;
 
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+
 use Doctrine\ORM\Mapping as ORM;
 
 /**
@@ -62,6 +65,15 @@ class User
      * @ORM\Column(name="created_at", type="datetime", nullable=true, options={"default"="NULL"})
      */
     private $createdAt = 'NULL';
+
+    /**
+     * @ORM\OneToMany(targetEntity="App\Entity\Task", mappedBy="user")
+     */
+    private $tasks;
+
+    public function __construct(){
+        $this->tasks = new ArrayCollection();
+    }
 
     public function getId(): ?int
     {
@@ -140,5 +152,12 @@ class User
         return $this;
     }
 
+    /**
+     * @return Collection|Task[]
+     */
+    public function getTasks(): Collection
+    {
+        return $this->tasks;
+    }
 
 }
