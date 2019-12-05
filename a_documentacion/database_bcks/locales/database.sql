007f038c844a414172fce763dbecbc056be5f385
Irene <ireneog_72@hotmail.es>
2019-12-05 20:34:31 +0100

    Create scripts to insert data

--- a_documentacion/database_bcks/locales/database.sql
+++ a_documentacion/database_bcks/locales/database.sql
@@ -12,6 +12,10 @@ CREATE TABLE IF NOT EXISTS users(
   CONSTRAINT pk_users PRIMARY KEY(id)
 )ENGINE=InnoDb;
 
+INSERT INTO users VALUES(NULL, 'ROLE_USER','Irene','Olgo','ire@ire.es','password', CURTIME());
+INSERT INTO users VALUES(NULL, 'ROLE_USER','Ariadna','Bool','ari@ari.com','password2', CURTIME());
+INSERT INTO users VALUES(NULL, 'ROLE_USER','Carlos','Perea','carlos@carlos.es','password3', CURTIME());
+
 CREATE TABLE IF NOT EXISTS tasks(
   id int (255) auto_increment not null,
   user_id int (255) not null,
@@ -23,3 +27,8 @@ CREATE TABLE IF NOT EXISTS tasks(
   CONSTRAINT pk_tasks PRIMARY KEY(id),
   CONSTRAINT fk_task_user FOREIGN KEY(user_id) REFERENCES users(id)
 )ENGINE=InnoDb;
+
+INSERT INTO tasks VALUES(NULL, 1,'Tarea 1','Contenido de prueba 1','hight',40, CURTIME());
+INSERT INTO tasks VALUES(NULL, 1,'Tarea 2','Contenido de prueba 2','hight',20, CURTIME());
+INSERT INTO tasks VALUES(NULL, 2,'Tarea 3','Contenido de prueba 3','hight',10, CURTIME());
+INSERT INTO tasks VALUES(NULL, 3,'Tarea 4','Contenido de prueba 4','hight',50, CURTIME());
