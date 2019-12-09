843535fb9609510e01137faa46e4e5158114184c
Irene <ireneog_72@hotmail.es>
2019-12-09 11:57:45 +0100

    Create Form User Register

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,15 +2,26 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+
+use App\Form\RegisterType;
+
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+
 class UserController extends AbstractController
 {
-    public function register()
+    public function register( Request $request )
     {
-        return $this->render('user/register.html.twig', [
+        $user = new User();
 
+        $form = $this->createForm( RegisterType::class, $user );
+
+        return $this->render('user/register.html.twig', [
+            'form' => $form->createView()
         ]);
     }
 }
