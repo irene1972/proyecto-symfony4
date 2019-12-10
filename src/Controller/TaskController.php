e2168629f1ebadae26bebaf543ad3beea60f580c
Irene <ireneog_72@hotmail.es>
2019-12-10 19:52:00 +0100

    Create twig & function & route for Create Task

--- src/Controller/TaskController.php
+++ src/Controller/TaskController.php
@@ -8,6 +8,9 @@ use App\Entity\Task;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+
 class TaskController extends AbstractController
 {
     public function index()
@@ -52,6 +55,11 @@ class TaskController extends AbstractController
     //         'email_' => $task->getUser()->getEmail()
     //     ]);
     // }
+
+    public function creation( Request $request ){
+
+        return $this->render('task/creation.html.twig', []);
+    }
 
     public function irene_test_relation_user_task(){
 
